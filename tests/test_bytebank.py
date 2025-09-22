b46a14be3580739ddd5fdebe9fbe8a2e08d51d05
KaBarbi <135548335+KaBarbi@users.noreply.github.com>
2025-09-22 16:01:54 -0300

    test decreescimo salario

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -21,3 +21,15 @@ class TestClass:
         resultado = kleber.sobrenome()
 
         assert resultado == esperado
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_nome = 'Paulo Bragança'
+        entrada_salario = 100000
+        esperado = 90000
+
+        funcionario_test = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+
+        funcionario_test.decrescimo_salario()
+        resultado = funcionario_test.salario
+
+        assert resultado == esperado
